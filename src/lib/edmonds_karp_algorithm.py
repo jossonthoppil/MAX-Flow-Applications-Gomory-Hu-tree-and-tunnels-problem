26f90620eec0398c3e798fb74995a47fe95e18f8
Mohit-Mithra <mohithmitra@gmail.com>
2020-06-20 11:41:44 +0530

    Streamlined edmond_karp algo

--- src/lib/edmonds_karp_algorithm.py
+++ src/lib/edmonds_karp_algorithm.py
@@ -1,22 +1,52 @@
 #Edmonds-Karp Algorithm
+from collections import deque
+from sys import maxsize as maxint
 
 #find path by using BFS
-def ek_bfs(C, F, s, t):
-		queue = [s]
-		paths = {s:[]}
-		if s == t:
-			return paths[s]
-		while queue: 
-			u = queue.pop(0)
-			for v in range(len(C)):
-					if(C[u][v]-F[u][v]>0) and v not in paths:
-						paths[v] = paths[u]+[(u,v)]
-						
-						if v == t:
-							print(paths[v])
-							return paths[v]
-						queue.append(v)
-		return None
+def ek_bfs(graph, start, target, flow):
+	
+	V = len(graph)
+
+	queue = deque()
+	queue.append(start)
+	paths = {start:[]}
+	if start == target:
+			return paths[start]
+
+	while queue:
+		u = queue.popleft()
+
+		for v in range(V):
+
+			if(graph[u][v]-flow[u][v]>0) and v not in paths:
+				paths[v] = paths[u]+[(u,v)]
+
+				if v == target:
+					print(paths[v])
+					return paths[v]
+
+				queue.append(v)
+
+	return None
+
+# def sanity_check(C, F, s, t):
+# 	"""This function is not used, please ignore it"""
+
+# 	queue = [s]
+# 	paths = {s:[]}
+# 	if s == t:
+# 		return paths[s]
+# 	while queue: 
+# 		u = queue.pop(0)
+# 		for v in range(len(C)):
+# 			if(C[u][v]-F[u][v]>0) and v not in paths:
+# 				paths[v] = paths[u]+[(u,v)]
+				
+# 				if v == t:
+# 					print(paths[v])
+# 					return paths[v]
+# 				queue.append(v)
+# 	return None
 
 
 def edmonds_karp_max_flow(C, s, t):
@@ -27,7 +57,7 @@ def edmonds_karp_max_flow(C, s, t):
 	for i in range(n):
 		F.append([0] * n)		
 	
-	path = ek_bfs(C, F, s, t)
+	path = ek_bfs(C, s, t, F)
  
 	while path != None:
 
@@ -37,7 +67,7 @@ def edmonds_karp_max_flow(C, s, t):
 			F[u][v] += flow
 			F[v][u] -= flow
 
-		path = ek_bfs(C, F, s, t)
+		path = ek_bfs(C, s, t, F)
 
 	final_flow = []
 	for i in range(n):
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from lib.edmonds_karp_algorithm import *
 from lib.push_relabel_algorithm import *
 from lib.dinic_algorithm import *
 
+from gomory_hu import GomoryHuTree
+
 def main():
 	# make a capacity graph
 	# node 0  1  2  3  4  5
@@ -34,6 +36,21 @@ def main():
 	max_flow_value = dinic_maxflow(C, source, sink)
 	print ("Dinic's algorithm")
 	print ("max flow value is: ", max_flow_value)
+
+	print()
+	print("Constructing the Gomory-Hu tree using Edmond-Karp algorithm")
+	tree = GomoryHuTree(C)
+
+	# Print Tree Contents
+	print(tree)
+
+	# Query for min cut between 0 and 4
+	print(tree.query(2, 0))
+	print(tree.query(0, 2))
+
+	# Print the edge (0, 3)
+	print(tree[0, 3])
+
 
 if __name__ == "__main__":
     main()
